1e876ce5cbf63a27b1bed425816cd5678ef13570
Gyt <igyteng@gmail.com>
2015-07-05 21:25:07 +0800

    weather

--- controller/weather.js
+++ controller/weather.js
@@ -2,6 +2,7 @@ app.controller('WeatherController', function($scope, $state, $window, $http) {
     $scope.home = function() {
         $state.go('photo');
     };
+    $scope.hideWeather = true;
     var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.placefinder%20where%20text%3D%22%E8%95%89%E5%B2%AD%22)%20and%20u%3D%22c%22&format=json&diagnostics=true&callback=';
     $http({
         method: 'GET',
@@ -11,6 +12,9 @@ app.controller('WeatherController', function($scope, $state, $window, $http) {
         //     format: 'json'
         // }
     }).success(function(data) {
-        console.log(data);
+        $scope.days = data.query.results.channel[0].item.forecast;
+        $scope.hideWeather = false;
     });
+
+
 });
